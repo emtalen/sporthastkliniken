aebf319f4363e212a654d27e217c59704fd748ed
emtalen <59577366+emtalen@users.noreply.github.com>
2020-05-02 15:08:36 +0200

    changing some styling since i followed the wrong layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div id='header'>
-      <h1>Sporthästkliniken Häljeby AB</h1>
+    <div id="header">
+      <div id="logo">
+        <h1>Sporthästkliniken Häljeby AB</h1>
+      </div>
+      <div id='menu'>Här kommer massa knappar, vänta du bara</div>
     </div>
-  )
+  );
 }
 
 export default Header
