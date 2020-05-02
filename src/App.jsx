cc9a1465c8b424869c53fcb58b2aa56f9367903f
emtalen <59577366+emtalen@users.noreply.github.com>
2020-05-02 13:47:17 +0200

    adding services component, not wuite happy with how it looks, will return to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import StartPage from './components/StartPage'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import StartPage from "./components/StartPage";
+import Services from "./components/Services";
 
 const App = () => {
   return (
     <>
       <Header />
       <StartPage />
+      <Services />
       <Footer />
     </>
   );
-}
+};
 
-export default App
-
+export default App;
