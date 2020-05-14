232557efdf8ea69f0792b4b4e51ecff4d437cd30
emtalen <59577366+emtalen@users.noreply.github.com>
2020-05-14 18:22:15 +0200

    fixing so header becomes filled when scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Services from "./components/Services";
 import Contact from './components/Contact'
 
 const App = () => {
+    
+
   return (
     <>
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import {NavLink} from 'react-router-dom';
 
 const Header = () => {
+
+    
+
   return (
     <div id="header">
       <div id="logo">
