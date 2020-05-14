c8f8bde9a1fa56c24680092392af0ffc0726e3b8
emtalen <59577366+emtalen@users.noreply.github.com>
2020-05-14 15:18:46 +0200

    adding images to the starting page and changing some backgroudn colors

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -3,6 +3,12 @@ import React from "react";
 const StartPage = () => {
   return (
     <div id="start-page">
+      {/* style=
+      {{
+        background: 'url("./public/images/0-bw.jpg")',
+        height: "100vh",
+        backgroundSize: "cover",
+      }} */}
       <div id="short-info">
         <h1>Här kommer ett jävligt smart citat från Laura</h1>
       </div>
