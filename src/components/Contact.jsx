276458accd5d1c2e52f5be6d9ed2fcb1c785bc61
emtalen <59577366+emtalen@users.noreply.github.com>
2020-05-14 17:09:40 +0200

    adding image and geeting on contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div id='contact'>
-      <div id='contact-info'>
+    <div id="contact">
+      <div id="contact-info">
         <h1>KONTAKT</h1>
-        <p>HÄSTSPORTKLINIKEN HÄLJEBY AB <br/>
-        Häljeby 4, 731 92 Köping</p>
+        <p>
+          HÄSTSPORTKLINIKEN HÄLJEBY AB <br />
+          Häljeby 4, 731 92 Köping
+        </p>
         <p>Måndag-Fredag 08.00-16.00</p>
-        <p>+46 70 378 4110 <br/>
-        Telefontid Vardagar 07.00-09.00</p>
+        <p>
+          +46 70 378 4110 <br />
+          Telefontid Vardagar 07.00-09.00
+        </p>
         <p>Övriga tider, skicka meddelande så ringer vi upp.</p>
       </div>
+      <div id='contact-greeting'>
+        <p>VI SES! <br/>
+        Team Sporthästkliniken Häljeby</p>
+        <img src="/images/46-bw.jpg" alt="team" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Contact
