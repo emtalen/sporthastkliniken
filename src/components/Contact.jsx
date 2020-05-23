967b807c029b3e090683bc899fb2f16fce5fa70f
emtalen <59577366+emtalen@users.noreply.github.com>
2020-05-23 19:47:37 +0200

    fixing with the contact page and the info box on the start pageä

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,69 +5,72 @@ const Contact = () => {
     <div id="contact">
       <div id="contact-info">
         <h1>KONTAKT</h1>
-        <p>
+        <div id="opening-hours">
+          <p
+            style={{
+              fontSize: "1.4rem",
+              textTransform: "uppercase",
+              fontWeight: "500",
+            }}
+          >
+            Öppettider
+          </p>
+          <p style={{ fontWeight: 500 }}>
+            Måndag, Onsdag-Fredag:
+            <p>Från 08:30</p>
+          </p>
+          <p style={{ fontWeight: 500, marginTop: "-0.5rem" }}>
+            Tisdag:
+            <p>
+              Jämn vecka STÄNGT (Laura opererar i Husaby, Skara)
+              <br />
+              Ojämn vecka från 12:00
+            </p>
+          </p>
+          <p style={{ fontWeight: 500, marginTop: "-0.5rem" }}>
+            Helg:
+            <p>STÄNGT</p>
+          </p>
+        </div>
+
+        <div id="booking">
+          <p style={{ fontSize: "1.4rem", fontWeight: "500" }}>
+            BOKNING <br />
+          </p>
+          <p style={{ fontSize: "1.3rem", fontWeight: "500" }}>
+            +46 70 378 4110
+          </p>
+          <p style={{ fontStyle: "italic", paddingTop: "0.5%" }}>
+            Om vi inte svarar, skicka ett SMS så ringer vi upp. <br />
+            OBS- Vi tar inte emot bokningar på email.
+          </p>
+
+          <p style={{ fontSize: "1.4rem" }}>
+            Bokning av operation
+            <p>
+              +46 511-275 50 <br /> skara@husabyhastkliniken.se
+            </p>
+          </p>
+        </div>
+        <p style={{ marginTop: "4%", fontSize: "1.4rem", fontWeight: "500" }}>
           SPORTHÄSTKLINIKEN HÄLJEBY AB <br />
           Häljeby 4, 731 92 Köping
           <br />
           info@sporthastkliniken.se
         </p>
-        <div id="opening-hours">
-          <p style={{fontSize: '1.4rem'}}>
-            Öppettider: <br />
-            <p style={{ fontStyle: "italic" }}>
-              Vänligen notera att våra öppettider varierar.
-            </p>
-          </p>
-          <div className="opening-hours-info">
-            <p style={{ fontWeight: 500 }}>
-              Mån, Ons, Tors, Fre:
-              <p>
-                Från 08:30 <p style={{ color: "lightGrey" }}> - </p>
-              </p>
-            </p>
-          </div>
-          <div id="opening-hours-info">
-            <p style={{ fontWeight: 500 }}>
-              Tis:
-              <p>
-                Jämn vecka STÄNGT (Laura opererar i Husaby, Skara)
-                <br />
-                Ojämn vecka från 12:00
-              </p>
-            </p>
-          </div>
-          <div className="opening-hours-info">
-            <p style={{ fontWeight: 500 }}>
-              Helg:
-              <p>
-                STÄNGT <p style={{ color: "lightGrey" }}>-</p>
-              </p>
-            </p>
-          </div>
-        </div>
-        <p style={{ fontSize: "1.5rem" }}>
-          BOKNING VIA TELEFON <br />
-          +46 70 378 4110
-        </p>
-        <p style={{ fontStyle: "italic" }}>
-          Om vi är upptagna, skicka ett SMS så ringer vi upp. <br />
-          OBS- Vi tar inte emot bokningar på email.
-        </p>
-        <p>Övriga tider, skicka meddelande så ringer vi upp.</p>
-        <p style={{ paddingTop: "2%", fontSize: "1.4rem" }}>
-          För bokningen av operation, kontakta Husby kliniken direkt
-          <p>
-            Tel: 0511-275 50 <br /> Mail: skara@husabyhastkliniken.se
-          </p>
-        </p>
+        <img src="images/Sporthastklininken_sv_RGB.png" alt="Logo" />
       </div>
-      <div id="contact-greeting">
-        <p>
-          VI SES! <br />
-          Team Sporthästkliniken Häljeby
-        </p>
-        <img src="/images/46-bw.jpg" alt="team" />
-      </div>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2020.8548844673699!2d16.094827951709988!3d59.56882529874901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465e809358575467%3A0x1a11b03c5e089e2d!2zSMOkbGplYnkgNCwgNzMxIDkyIEvDtnBpbmc!5e0!3m2!1sen!2sse!4v1590253591363!5m2!1sen!2sse"
+        width="100%"
+        height="300"
+        frameborder="0"
+        style={{border:"0"}}
+        allowfullscreen=""
+        aria-hidden="false"
+        tabindex="0"
+      ></iframe>
     </div>
   );
 };
--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -3,21 +3,19 @@ import React from "react";
 const StartPage = () => {
   return (
     <div id="start-page">
-      {/* style=
-      {{
-        background: 'url("./public/images/0-bw.jpg")',
-        height: "100vh",
-        backgroundSize: "cover",
-      }} */}
       <div id="short-info">
         <h1>
-          Vi är en hästklinik med fokus på sporthästen med lång erfarenhet från
-          akutsjukvård, ortopedi och kirurgi.<br/> Vi samarbetar med duktiga
-          hovslagare, fysioterapeuter och rehabiliteringsanläggningar för att
-          hitta en helheltslösning för dig och din häst.<br/> Vi håller oss i
-          framkant när det gäller diagnostiska tekniker och behandling av olika
-          skador.
+          Välkommen till Sporthästkliniken! <br />
         </h1>
+        <h3>
+          Vi är en hästklinik med fokus på sporthästen med lång erfarenhet från
+          akutsjukvård, ortopedi och kirurgi.
+          <br /> Vi samarbetar med duktiga hovslagare, fysioterapeuter och
+          rehabiliteringsanläggningar för att hitta en helheltslösning för dig
+          och din häst.
+          <br /> Vi håller oss i framkant när det gäller diagnostiska tekniker
+          och behandling av olika skador.
+        </h3>
       </div>
     </div>
   );
