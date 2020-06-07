2fa7e62704e8ac857bd077a34fc3853564a53dcb
emtalen <59577366+emtalen@users.noreply.github.com>
2020-06-07 21:37:35 +0200

    changing the team texts

--- src/components/TheTeam.jsx
+++ src/components/TheTeam.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const TheTeam = () => {
   return (
@@ -10,31 +10,38 @@ const TheTeam = () => {
           <div className="team-member-container">
             <h2>Laura Hirvinen</h2>
             <p>
-              Startade Equiaid Veterinär AB i början av 2016 med fokus på
-              sporthästortopedi och som konsulterande kirurg. 2020 bytte Equiaid
-              namn till Hästsportkliniken Häljeby AB. Laura har i över tio år
-              jobbat på olika hästsjukhus i USA, Sverige och Finland.
+              “If you can dream it, you can do it”. Laura var 8 år gammal när
+              hon ritade planritning på sin veterinärklinik. 2004 började hon
+              jobba som veterinär, veterinärexamen tog hon från Helsingfors
+              Universitet. Året 2005 började hon som intern på ett privat
+              hästsjukhus i Finland men drömmarna var större redan då. <br />
+              Året därpå kom hon in på en specialistutbildning inom hästkirurgi
+              vid The Ohio State University, USA. Under dessa tre år fick hon
+              jobba med, och lära sig från världsledande kirurger,
+              anestesiologer, intermedicinare och intesivvårdsspecialister inom
+              häst. Det gav henne en unik kompetens inom hästkirurgi samt enorm
+              klinisk erfarenhet inom alla hästmedicinens olika grenar. Året
+              2010 genomförde hon den svåra ACVS examen i SanDiego, Kalifornien
+              och blev därmed den första skandinaviska veterinären med titeln
+              Diplomate of American College of Veterinary Surgeons (DiplACVS).
               <br />
-              Hon flyttade till Sverige 2009 och jobbade mellan 2009 - 2015 på
+              Laura flyttade till Sverige 2009 och jobbade mellan 2009 - 2015 på
               Evidensia Specialisthästsjukhuset Strömsholm, varav den senaste
               tiden som överveterinär i kirurgi. Under hennes tid på Evidensia
-              jobbade hon även som konsult på många olika kliniker runt om i
-              Sverige. Laura genomförde sin specialistutbildning inom
-              hästkirurgi vid The Ohio State University mellan 2006-2009 och är
-              den enda i Sverige med titeln Diplomate of American College of
-              Veterinary Surgeons (DiplACVS).
-              <br />I USA fick hon möjligheten att jobba och lära sig från
-              världsledande kirurger, anestesiologer, intermedicinare och
-              intesivvårdsspecialister inom häst. Det gav henne en unik
-              kompetens inom hästkirurgi samt enorm klinisk erfarenhet inom alla
-              hästmedicinens olika grenar.
-              <br />
-              Idag samarbetar Laura med duktiga hovslagare, fysioterapeuter,
-              naprapater, och tränare. Utöver undersökningar och operationer
-              håller hon många föreläsningar på olika konvent runt om i landet.
-              Hon äger själv fyra hästar och tränar och tävlar flitigt i
-              dressyr. Laura samarbetar bland annat med Maria Terese Engell och
-              Anna-Clara Pettersson med fokus på ryttarens inverkan på hästen.
+              jobbade hon även som konsult på många olika Evidensia kliniker
+              runt om i Sverige. <br /> Drömmen om något eget började växa fram
+              under 2015 och i början av 2016 startade hon sin egen praktik
+              under namnet Equiaid Veterinär AB. Den ambulatoriska verksamheten
+              fick snabbt konverteras till ortopedimottagning på Equirehabs
+              lokaler i Kvicksund. Utöver detta jobbade hon som konsulterande
+              kirurg på Husaby Hästakut i Skara och på SLU i Uppsala. En snabb
+              växande kundkrets och EU-stöd möjliggjorde den långvariga drömmen
+              om egen klinik. <br /> Laura startade Sporthästkliniken Häljeby AB
+              på sin gård den 1 juni 2020. Hon fortsätter att operera på Husaby
+              Hästakut i Skara varannan vecka på tisdagar. Utöver detta är hon
+              flitigt anlitat föreläsare av Svenska Ridsportförbundet. Hemma på
+              gården har hon 5 hästar och hon syns ibland på dressyrbanorna med
+              sina hästar.
             </p>
           </div>
         </div>
@@ -43,11 +50,12 @@ const TheTeam = () => {
           <div className="team-member-container">
             <h2>Mika Thalén</h2>
             <p>
-              Företagets spindel i nätet. Mika började jobba på Equiaid 2018.
-              Hon driver en hästgård med sin man utanför Strömsholm. Hon har sin
-              häst Aida som hon tävlar hoppning med. Mika sköter alla bokningar
-              och all kundkontakt som sker via sms, telefon och mejl. Hon jobbar
-              även med Laura ute på Sporthästkliniken varannan Tisdag.
+              Företagets spindel i nätet. Mika började jobba på
+              Sporthästkliniken 2018. Hon driver en hästgård med sin man utanför
+              Strömsholm där hon har sin häst Aida som hon tävlar hoppning med.
+              Mika sköter alla bokningar och all kundkontakt som sker via sms,
+              telefon och mejl och finns ofta på plats på Sporthästkliniken
+              tillsammans med Laura.
             </p>
           </div>
         </div>
@@ -59,12 +67,12 @@ const TheTeam = () => {
               Emelie började jobba på Sporthästkliniken i December 2017. Hon har
               en hästgård ca en timma från Strömsholm med lite hobbyhästar,
               hundar, katter och älgar inpå knuten. Emelie är mammaledig men har
-              nu börjat mjukstarta med att jobba Torsdagar varje vecka.
+              nu börjat mjukstarta med att jobba torsdagar varje vecka.
             </p>
           </div>
         </div>
         <div className="team-member">
-          <img src="/images/IMG_4576.PNG" alt="Magnus" id='magnus-image'/>
+          <img src="/images/IMG_4576.PNG" alt="Magnus" id="magnus-image" />
           <div className="team-member-container">
             <h2>Magnus Smederöd </h2>
             <p>
@@ -72,15 +80,15 @@ const TheTeam = () => {
               har examen i SHF Forbildning. Magnus innehar Mästarbrev sedan
               2015. Erfarenhet och skor flera discipliner; Dressyr, Hoppning,
               Fälttävlan, Reining och Trav. Magnus utför även föl korregering
-              och ortopediska beslag på häst. Sedan 2019 jobbar han med
-              Black-Werkman Hoofcare, ett system som mäter hästens rörelse, steg
-              och hovens olika belastningsfaser.
+              och sjukbeslag på häst. Sedan 2019 jobbar han med Black-Werkman
+              Hoofcare, ett system som mäter hästens rörelse, steg och hovens
+              olika belastningsfaser.
             </p>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default TheTeam
+export default TheTeam;
